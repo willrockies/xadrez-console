8eb913b1c9fbe113928a24fdfb6d01c38bcb2c02
Wilson Alves <wlsn.ads@gmail.com>
2021-01-27 15:00:18 -0300

    Add project files.

---
+++ xadrez-console/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using tabuleiro;
+
+namespace xadrez_console
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Posicao p;
+
+            p = new Posicao(3, 4);
+
+            Console.WriteLine(p);
+
+            Console.ReadLine();
+        }
+    }
+}
