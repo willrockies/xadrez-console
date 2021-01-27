029fd7524f911e9f3093b84a1bf913be0c9d4342
Wilson Alves <wlsn.ads@gmail.com>
2021-01-27 17:48:46 -0300

    Colocando peças no tabuleiro

--- xadrez-console/tabuleiro/Posicao.cs
+++ xadrez-console/tabuleiro/Posicao.cs
@@ -4,20 +4,20 @@ namespace tabuleiro
 {
     class Posicao
     {
-        public int Linha { get; set; }
-        public int Coluna { get; set; }
+        public int linha { get; set; }
+        public int coluna { get; set; }
 
         public Posicao(int linha, int coluna)
         {
-            Linha = linha;
-            Coluna = coluna;
+            this.linha = linha;
+            this.coluna = coluna;
         }
 
         public override string ToString()
         {
-            return Linha
+            return linha
                 + ", "
-                + Coluna;
+                + coluna;
         }
     }
 }
